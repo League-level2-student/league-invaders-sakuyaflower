b1f6512f90cd0fd526136ad513a17a865da2370a
league <league@imac-5.attlocal.net>
2019-10-19 11:28:58 -0700

    League Invader, GamePanel(Up to the Change the State)

---
+++ src/GamePanel.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	@Override
+	public void paintComponent(Graphics g) {
+		final int MENU = 0;
+		final int GAME = 1;
+		final int END = 2;
+		int currentState = MENU;
+
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
+		}
+	}
+
+	JPanel JP = new JPanel();
+
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+public static final int WIDTH = 0;
+public static final int HEIGHT = 0;
+GamePanel game = new GamePanel();
+	public static void main(String[] args) {
+	LeagueInvaders invaders = new LeagueInvaders();
+	invaders.setup();
+	
+}
+void setup() {	
+	
+	JFrame JF = new JFrame();
+	JF.add(game);
+	JF.setSize(500, 800);
+	JF.setVisible(true);
+}
 }
